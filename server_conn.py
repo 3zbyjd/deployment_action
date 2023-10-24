da31d5bba7c7082db7a132b8084eb20c9d50d4c6
demond6374 <demond.roberson@gmail.com>
2023-10-24 04:08:15 +0000

    "Made attempt to connect with key instead of password"

--- server_conn.py
+++ server_conn.py
@@ -1,6 +1,7 @@
-import paramiko
 from configparser import ConfigParser
+import os.path
 import time
+import paramiko
 
 #Read config.ini file
 config_object = ConfigParser()
@@ -12,18 +13,29 @@ serverconfig = config_object["SERVERCONFIG"]
 host = serverconfig["host"]
 username = serverconfig["username"]
 password = serverconfig["password"]
+sshKeyFilename = os.path.join(os.path.expanduser('~'), ".ssh", "id_ed25519")
+sshKeyPassphrase = serverconfig["sshKeyPassphrase"]
 
 #print("Server IP address = {}".format(host))
 #print("Password = {}".format(password))
 
-clientSession = paramiko.SSHClient();
+clientSession = paramiko.SSHClient()
 clientSession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 clientSession.connect(hostname=host,
+                      look_for_keys=True,
                       username=username,
-                      password=password)
+                      password=password,
+                      key_filename=sshKeyFilename,
+                      passphrase=sshKeyPassphrase)
 
-stdin, stdout, stderr = clientSession.exec_command('hostname')
+stdin, stdout, stderr = clientSession.exec_command('sudo cd /home/demond/.ssh')
 time.sleep(.5)
 print(stdout.read().decode())
+print(stderr.read().decode())
+
+stdin, stdout, stderr = clientSession.exec_command('pwd')
+time.sleep(.5)
+print(stdout.read().decode())
+print(stderr.read().decode())
 
 clientSession.close()
