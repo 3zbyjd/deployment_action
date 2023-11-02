4f734361b45785ac06a751b5b5d63d322a23d8be
demond6374 <demond.roberson@gmail.com>
2023-11-01 20:49:56 -0500

    Made changes to shift to another directory for publishing site to production

--- server_conn.py
+++ server_conn.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser
 import time
 import paramiko
+import select
 
 # Read config.ini file
 config_object = ConfigParser()
@@ -20,9 +21,22 @@ clientSession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshpkey = paramiko.RSAKey.from_private_key_file(sshKeyFilename, sshKeyPassphrase)
 clientSession.connect(hostname=host, username=username, pkey=sshpkey)
 
-stdin, stdout, stderr = clientSession.exec_command("pwd")
-time.sleep(0.5)
-print(stdout.read().decode())
-print(stderr.read().decode())
+commands = ["cd www/rlomuniv/", "pwd"]
+
+for cmd in commands:
+    stdin, stdout, stderr = clientSession.exec_command(cmd)
+
+    while not stdout.channel.exit_status_ready():
+        if stdout.channel.recv_ready():
+            rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+            if len(rl) > 0:
+                tmp = stdout.channel.recv(1024)
+                output = tmp.decode()
+                print(output)
+
+    # time.sleep(1)
+    # stdout
+    # print(stdout.read().decode())
+    # print(stderr.read().decode())
 
 clientSession.close()
