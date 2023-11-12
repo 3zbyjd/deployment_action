ec9fb7fc2f678c0ed35553f835884c0a416ba715
demond6374 <demond.roberson@gmail.com>
2023-11-11 22:43:54 -0600

    Testing os.walk

--- server_conn.py
+++ server_conn.py
@@ -24,79 +24,6 @@ sshClient = paramiko.SSHClient()
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshpkey = paramiko.RSAKey.from_private_key_file(sshKeyFilename, sshKeyPassphrase)
 
-
-def recurseContents(contentDir, contentList):
-    fileExistsTF = False
-
-    parentDirectory = contentDir
-    workingDirectory = parentDirectory
-
-    for contentItem in contentList:
-        """if os.path.isfile(contentItem):
-        sftpClient.put(contentItem, sftpRemoteDirectory)"""
-        splitItems = contentItem.split(".")
-        if len(splitItems) == 2:
-            if splitItems[1] in (
-                "gif",
-                "jpeg",
-                "jpg",
-                "png",
-                "js",
-                "html",
-                "htm",
-                "json",
-                "exe",
-                "pdb",
-                "dll",
-                "pdf",
-            ):
-                sftpClient.put(contentItem, sftpRemoteDirectory)
-        elif len(splitItems) == 3:
-            if splitItems[2] in (
-                "gif",
-                "jpeg",
-                "jpg",
-                "png",
-                "js",
-                "html",
-                "htm",
-                "json",
-                "exe",
-                "pdb",
-                "dll",
-                "pdf",
-            ):
-                sftpClient.put(contentItem, sftpRemoteDirectory)
-
-        else:
-            try:
-                sftpClient.stat(contentItem)
-            except IOError as e:
-                if "No such file" in str(e):
-                    fileExistsTF = False
-            else:
-                fileExistsTF = True
-
-            if not fileExistsTF:
-                sftpClient.mkdir(contentItem, 755)
-                sftpClient.chown(contentItem, 1000, 1000)
-                sftpClient.chdir(contentItem)
-                # Print current working directory
-                print(sftpClient.getcwd())
-            else:
-                sftpClient.chdir(contentItem)
-                # Print current working directory
-                print(sftpClient.getcwd())
-
-            workingDirectory = os.path.join(workingDirectory, contentItem)
-            subContentList = os.listdir(workingDirectory)
-            if len(subContentList) > 0:
-                recurseContents(workingDirectory, subContentList)
-            sftpClient.chdir("../")
-
-    pass
-
-
 try:
     # Establish ssh connection to remote server
     print("Attempting to establish ssh connection")
@@ -115,12 +42,129 @@ try:
     # Print current working directory
     print(sftpClient.getcwd())
 
-    dirContentList = os.listdir(sftpLocalDirectory)
-    if len(dirContentList) > 0:
-        recurseContents(sftpLocalDirectory, dirContentList)
+    for rootPath, dirs, fileItem in os.walk(sftpLocalDirectory):
+        for f in fileItem:
+            if f in (
+                "gif",
+                "jpeg",
+                "jpg",
+                "png",
+                "js",
+                "html",
+                "htm",
+                "json",
+                "exe",
+                "pdb",
+                "dll",
+                "pdf",
+            ):
+                print(f)
 
 except:
     print("[!] Connection attempt failed")
     exit()
+
+
+# def recurseContents(contentDir, remoteContentDir, contentList):
+#     fileExistsTF = False
+
+#     parentDirectory = contentDir
+#     workingDirectory = parentDirectory
+#     remoteWorkingDirectory = remoteContentDir
+
+#     for contentItem in contentList:
+#         """if os.path.isfile(contentItem):
+#         sftpClient.put(contentItem, sftpRemoteDirectory)"""
+#         splitItems = contentItem.split(".")
+#         if len(splitItems) == 2:
+#             if splitItems[1] in (
+#                 "gif",
+#                 "jpeg",
+#                 "jpg",
+#                 "png",
+#                 "js",
+#                 "html",
+#                 "htm",
+#                 "json",
+#                 "exe",
+#                 "pdb",
+#                 "dll",
+#                 "pdf",
+#             ):
+#                 sftpClient.put(contentItem, remoteWorkingDirectory)
+#         elif len(splitItems) == 3:
+#             if splitItems[2] in (
+#                 "gif",
+#                 "jpeg",
+#                 "jpg",
+#                 "png",
+#                 "js",
+#                 "html",
+#                 "htm",
+#                 "json",
+#                 "exe",
+#                 "pdb",
+#                 "dll",
+#                 "pdf",
+#             ):
+#                 sftpClient.put(contentItem, remoteWorkingDirectory)
+
+#         else:
+#             try:
+#                 sftpClient.stat(contentItem)
+#             except IOError as e:
+#                 if "No such file" in str(e):
+#                     fileExistsTF = False
+#             else:
+#                 fileExistsTF = True
+
+#             if not fileExistsTF:
+#                 sftpClient.mkdir(contentItem, 755)
+#                 sftpClient.chown(contentItem, 1000, 1000)
+#                 sftpClient.chdir(contentItem)
+#                 # Print current working directory
+#                 print(sftpClient.getcwd())
+#             else:
+#                 sftpClient.chdir(contentItem)
+#                 # Print current working directory
+#                 print(sftpClient.getcwd())
+
+#             workingDirectory = os.path.join(workingDirectory, contentItem)
+#             remoteWorkingDirectory = remoteWorkingDirectory + "/" + contentItem
+#             subContentList = os.listdir(workingDirectory)
+#             if len(subContentList) > 0:
+#                 recurseContents(
+#                     workingDirectory, remoteWorkingDirectory, subContentList
+#                 )
+#             sftpClient.chdir("../")
+
+#     pass
+
+
+# try:
+#     # Establish ssh connection to remote server
+#     print("Attempting to establish ssh connection")
+#     sshClient.connect(hostname=host, username=username, pkey=sshpkey)
+#     # sshClient.connect(hostname=host, username=username, password=sshPassword)
+#     print("Connection successfully established with", host)
+
+#     # Established sftp connection
+#     print("Transitioning to SFTP client")
+#     sftpClient = sshClient.open_sftp()
+#     print("Successfully transitioned to SFTP client")
+
+#     # Changing directory to target directory
+#     sftpClient.chdir(sftpRemoteDirectory)
+
+#     # Print current working directory
+#     print(sftpClient.getcwd())
+
+#     dirContentList = os.listdir(sftpLocalDirectory)
+#     if len(dirContentList) > 0:
+#         recurseContents(sftpLocalDirectory, sftpRemoteDirectory, dirContentList)
+
+# except:
+#     print("[!] Connection attempt failed")
+#     exit()
 
 sshClient.close()
