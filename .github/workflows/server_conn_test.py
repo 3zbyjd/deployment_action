52dcdedf124ed22a0ef520f3a1780754344f043f
demond6374 <demond.roberson@gmail.com>
2023-11-30 20:05:32 -0600

    Added setup python back to code

--- .github/workflows/server_conn_test.py
+++ .github/workflows/server_conn_test.py
@@ -1,17 +1,5 @@
 import os
 
-print("Enter here")
 
-
-def main():
-    print("Hello from GitHub Actions!")
-    host = os.environ.get("HOST")
-
-    if not host:
-        raise RuntimeError("HOST env var is not set!")
-    print("Host = ", host)
-
-    if __name__ == "__main__":
-        main()
-    else:
-        print("Procedure did not run")
+if __name__ == "__main__":
+    print("Python code reached")
