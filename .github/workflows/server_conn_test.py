910518254ddad87b00744974d1120fd929818856
demond6374 <demond.roberson@gmail.com>
2023-12-01 22:49:34 -0600

    Running test python

--- .github/workflows/server_conn_test.py
+++ .github/workflows/server_conn_test.py
@@ -3,12 +3,6 @@ import os
 
 def main():
     print("Hello from GitHub Actions!")
-    host = os.environ.get("HOST")
-
-    if not host:
-        raise RuntimeError("HOST env var is not set!")
-    print("Host = ", host)
-
 
 if __name__ == "__main__":
     main()
